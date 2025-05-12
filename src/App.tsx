f0d1f96f0eee05cff4956f685d987249119e2105
Mehdi Jeyrani <mehdijeyrani@gmail.com>
2025-05-12 13:42:10 +0330

    Add complete registration form with multiple validation rules using Yup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import SimpleForm from "./components/SimpleForm";
+import RegisterForm from "./components/RegisterForm";
 
 const App: React.FC = () => {
   return (
     <div className="p-5">
       <SimpleForm />
+      <hr />
+      <RegisterForm />
     </div>
   );
 };
