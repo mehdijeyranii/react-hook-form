49de120cd7e4ddc75b033cf7b2b33d2cf07691e0
Mehdi Jeyrani <mehdijeyrani@gmail.com>
2025-05-12 16:13:04 +0330

    Add book submission form with number and date validations using Yup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import SimpleForm from "./components/SimpleForm";
 import RegisterForm from "./components/RegisterForm";
+import BookForm from "./components/BookForm";
 
 const App: React.FC = () => {
   return (
@@ -8,6 +9,8 @@ const App: React.FC = () => {
       <SimpleForm />
       <hr />
       <RegisterForm />
+      <hr />
+      <BookForm />
     </div>
   );
 };
