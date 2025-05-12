4b79cd9cc0cfdc0e067c84ca10b23aa51740e58b
Mehdi Jeyrani <mehdijeyrani@gmail.com>
2025-05-12 12:56:48 +0330

    Add custom error message for required fields

--- src/components/SimpleForm.tsx
+++ src/components/SimpleForm.tsx
@@ -6,7 +6,11 @@ type FormData = {
 };
 
 const SimpleForm = () => {
-  const { register, handleSubmit } = useForm<FormData>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
 
   const onSubmit = (data: FormData) => {
     console.log(data);
@@ -16,11 +20,23 @@ const SimpleForm = () => {
     <form onSubmit={handleSubmit(onSubmit)}>
       <div>
         <label htmlFor="firstName">First Name:</label>
-        <input {...register("firstName")} className="border" />
+        <input
+          {...register("firstName", {
+            required: "First Name is required!",
+          })}
+          className="border"
+        />
+        {errors.firstName && <p>{errors.firstName.message}</p>}
       </div>
       <div>
         <label htmlFor="lastName">Last Name:</label>
-        <input {...register("lastName")} className="border" />
+        <input
+          {...register("lastName", {
+            required: "Last Name is required!",
+          })}
+          className="border"
+        />
+        {errors.lastName && <p>{errors.lastName.message}</p>}
       </div>
       <button type="submit">Submit</button>
     </form>
