a64bcb2d83c57386f611707b029fc090b52f1065
Mehdi Jeyrani <mehdijeyrani@gmail.com>
2025-05-14 17:58:03 +0330

    feat: add new user via POST request and update user list

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,24 +1,34 @@
 import { useEffect, useState } from "react";
 import type { IUser } from "../types/user";
+import AddUserForm from "./AddUserForm";
 
 const UserList = () => {
   const [users, setUsers] = useState<IUser[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
 
-  useEffect(() => {
+  const fetchUsers = () => {
     fetch("http://localhost:5000/users")
       .then((res) => res.json())
       .then((data: IUser[]) => {
         setUsers(data);
         setLoading(false);
       });
+  };
+
+  useEffect(() => {
+    fetchUsers();
   }, []);
+
+  const handleUserAdded = (user: IUser) => {
+    setUsers((prev) => [...prev, user]);
+  };
 
   if (loading) return <p className="text-3xl bg-amber-100 p-5">Loading...</p>;
 
   return (
-    <div className="bg-amber-200 p-5">
+    <div className="bg-amber-100 p-5">
       <h2 className="text-xl text-neutral-800">Users List</h2>
+      <AddUserForm onUserAdded={handleUserAdded} />
       <ul>
         {users.map((user) => (
           <li key={user.id}>
